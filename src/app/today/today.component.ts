d0ddd807bb4d1f009203d00d6f5652ffbe82e1b6
dakadabra <david.a.khazzam@gmail.com>
2020-01-25 19:29:44 -0500

    Added basic fetch c@sh$$ baby;

--- src/app/today/today.component.ts
+++ src/app/today/today.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// let photoUrl;
+
+// fetch('https://api.shutterstock.com/v2/images/search?query=donutdog', {
+//   headers: {
+//     Authorization: 'Basic d211NktDdmViTk1leXJ0Z0tBa2JPRXlUejFpa3ZwMnA6RGRacVpNWTZCcUk5dVpQZQ=='
+//   }
+  
+// }).then(response => response.json()).then((myJson) => {
+//   photoUrl = myJson.data[0].assets.preview.url;
+//   });
+
+// console.log(photoUrl);
+
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
@@ -11,5 +24,26 @@ export class TodayComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  var photoUrl:string;
+  
+  function onClick(keywords) {
+
+    fetch('https://api.shutterstock.com/v2/images/search?query=' + keywords, {
+      headers: {
+        Authorization: 'Basic d211NktDdmViTk1leXJ0Z0tBa2JPRXlUejFpa3ZwMnA6RGRacVpNWTZCcUk5dVpQZQ=='
+      }
+      
+    }).then(response => response.json()).then((myJson) => {
+      photoUrl = myJson.data[0].assets.preview.url;
+      });
+    
+    
+  }
+
+  onClick("cash money")
+
+  console.log(photoUrl);
+  
 
 }
